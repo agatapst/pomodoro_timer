31991b1905725314e67b4017e0904a208e7c9943
Agata Stawarz <agata.stawarz94@gmail.com>
2019-03-21 13:10:49 +0100

    Add reset feature

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 console.log("Connected");
 const btnStart = document.querySelector("#start");
 const btnPause = document.querySelector("#pause");
+const btnReset = document.querySelector("#reset")
 
+var newline = "\r\n";
 var startClicked = false;
 var timePaused = false;
+var timeReset = false; 
 var workCycle = 0;
+var timeInterval;
+var display = document.querySelector("#timerText");
 
 btnStart.addEventListener("click", function() {
+    // Timer was clicked before
+    if (startClicked){
+        timePaused = false;
+        console.log("button -start- clicked more than once")
     // Timer was not clicked before
-    if (!startClicked){
+    } else {
         startClicked = true;
         startTimer();
         console.log("button -start- clicked once")
-        
-        // Timer was clicked before
-    } else {
-        timePaused = false;
-        console.log("button -start- clicked more than once")
     }
 }); 
 
-
 // Start timer for the first time
 function startTimer(){
-    var duration = 25 * 60;
-    var display = document.querySelector("#time");
-    var timeInterval = setInterval(function() {
+    var duration = 1/6 * 60;
+    timeInterval = setInterval(function() {
         console.log("timePaused:", timePaused);
         if(!timePaused){
             duration--;
-            display.textContent = timeView(duration);
+            display.textContent = "let's work for" + newline + timeView(duration) + newline + " minutes";
             
             if (duration <= 0) {
                 console.log("break!")
@@ -42,17 +44,15 @@ function startTimer(){
                     startBreak();
                     console.log("short break");
                 }
-            } 
-        } 
-    }, 1000);        
+            }    
+        }   
+    }, 1000);     
 }
-
 
 // Display timer
 function timeView(time) {
     var minutes = parseInt(time / 60, 10);
     var seconds = parseInt(time % 60, 10); 
-    
     if (seconds >= 10){
         return minutes + ":" + seconds;
     } else {
@@ -68,34 +68,42 @@ btnPause.addEventListener("click", function(){
 // Break timer [short]
 function startBreak(){
     var breakDuration = 5 * 60;
-    var display = document.querySelector("#break");
-    
-    var breakInterval = setInterval(function() {
-        breakDuration--;
-        display.textContent = timeView(breakDuration);
+    timeInterval = setInterval(function() {
+        if(!timePaused){
+            breakDuration--;
+            display.textContent = "let's rest for" + newline + timeView(breakDuration) + newline + " minutes";
         
-        if (breakDuration <= 0) {
-            console.log("the end of break!")
-            clearInterval(breakInterval);
-            startTimer();
-        } 
+            if (breakDuration <= 0) {
+                console.log("the end of break!")
+                clearInterval(timeInterval);
+                startTimer();
+            } 
+        }
     }, 1000);
 }
 
 // Break timer [long]
 function startBreakLong(){
     var breakDurationLong = 15 * 60;
-    var display = document.querySelector("#break");
-    
-    var breakIntervalLong = setInterval(function() {
+    timeInterval = setInterval(function() {
+        if(!timePaused){
         breakDurationLong--;
-        display.textContent = timeView(breakDurationLong);
+        display.textContent = "let's rest for" + newline + timeView(breakDurationLong) + newline + " minutes";
         
-        if (breakDurationLong <= 0) {
-            console.log("the end of long break!")
-            clearInterval(breakIntervalLong);
-            startTimer();
+            if (breakDurationLong <= 0) {
+                console.log("the end of long break!")
+                clearInterval(timeInterval);
+                startTimer();
+            }
         } 
     }, 1000);
 }
+
+// Reset
+btnReset.addEventListener("click", function(){
+    display.textContent = "Okay, focus again!"
+    clearInterval(timeInterval);
+    startClicked = false; 
+});
+
 
